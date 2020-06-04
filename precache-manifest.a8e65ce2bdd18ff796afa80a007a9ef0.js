0ba8f327772d6199f30e0f5ba4bbe0d1c81531d4
michael-916310 <916310@rambler.ru>
2020-06-04 14:11:18 +0400

    add

---
+++ precache-manifest.a8e65ce2bdd18ff796afa80a007a9ef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d705ea567bfb1d65e8c5094275acb106",
+    "url": "/EpamCourse-ReactOnly-Deploy/index.html"
+  },
+  {
+    "revision": "12e0a6e6f3fade0aad53",
+    "url": "/EpamCourse-ReactOnly-Deploy/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f8ed96d2bd574a33c233",
+    "url": "/EpamCourse-ReactOnly-Deploy/static/css/main.c4d59ff8.chunk.css"
+  },
+  {
+    "revision": "12e0a6e6f3fade0aad53",
+    "url": "/EpamCourse-ReactOnly-Deploy/static/js/2.8d639ac4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/EpamCourse-ReactOnly-Deploy/static/js/2.8d639ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ed96d2bd574a33c233",
+    "url": "/EpamCourse-ReactOnly-Deploy/static/js/main.820fc348.chunk.js"
+  },
+  {
+    "revision": "6fb180707d293f796e5e",
+    "url": "/EpamCourse-ReactOnly-Deploy/static/js/runtime-main.cf0aee1e.js"
+  }
+]);
